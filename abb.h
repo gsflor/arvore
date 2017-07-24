cab2549f44c45f3bae49ecc4672c26e01fe6bba7
Gabriel Flor <gabriel.flor@outlook.com.br>
2017-07-23 23:02:16 -0300

    Update abb.h

--- abb.h
+++ abb.h
@@ -7,17 +7,15 @@ Arv* criavazia();
 
 Arv* criaArv(int key, Arv* esq, Arv* dir);
 
-Arv* chkVazia(Arv* p);
+int chkVazia(Arv* p);
 
 Arv* libera(Arv* p);
 
-Arv* chkKey(Arv* p, int key);
+int chkKey(Arv* p, int key);
 
 void imprime(Arv* p);
 
 int seekMin(Arv* p);
-
-Arv* busca (arvore r, int k);
 
 Arv* buscaMin(Arv* p);
 
