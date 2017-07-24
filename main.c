d5a53b257fa197a13b9e918a7b191377e8dbc1cf
Gabriel Flor <gabriel.flor@outlook.com.br>
2017-07-23 23:05:48 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,110 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
-
-typedef struct arvore Arv;
-
-struct arvore{
-    int key;
-    Arv* esq;
-    Arv* dir;
-};
-
-typedef struct arvore Arv;
-
-Arv* criavazia(){
-    return NULL;
-};
-
-Arv* criaArv(int key, Arv* esq, Arv* dir){
-    Arv* p = (Arv*)malloc(sizeof(Arv));
-    p->key = key;
-    p->esq = esq;
-    p->dir = dir;
-    return p;
-}
-
-Arv* chkVazia(Arv* p){
-    return p==NULL;
-}
-
-Arv* libera(Arv* p){
-    if(!chkVazia(p)){
-        free(p->esq);
-        free(p->dir);
-        free(p);
-    }
-    return NULL;
-    
-}
-
-Arv* chkKey(Arv* p, int key){
-    if(chkVazia(p)){
-        return 0;
-    } else{
-        return p->key||p->esq->key||p->dir->key;
-    }
-}
-
-void imprime(Arv* p){
-    if(!chkVazia(p)){
-        printf("%d",p->key);
-        printf("%d",p->dir->key);
-        printf("%d",p->esq->key);
-    }
-}
-
-int seekMin(Arv* p){
-    int minKey = 99999;
-    Arv* l1,r1;
-    l1 = p->esq;
-    r1 = p->dir;
-    if(p->key<minKey){
-        minKey = p->key;
-    }
-    while(!chkVazia(l1)){
-     if(l1->key<minKey){
-         minKey = l1->key;
-         if(!chkVazia(l1->esq)){
-             l1 = l1->esq;
-         }
-     }   
-    }
-}
-
-Arv* busca (arvore r, int k) {
-    if (r == NULL || r->chave == k)
-       return r;
-    if (r->chave > k)
-       return busca (r->esq, k);
-    else
-       return busca (r->dir, k);
-}
-
-Arv* buscaMin(Arv* p){
-    int min = 99999;
-    if(chkVazia(p)==NULL){
-        return NULL;
-    }
-    if(p->key<min){
-        min = p->key;
-        buscaMin(p->esq);
-        buscaMin(p->dir);
-    }
-}
+#include "abb.h"
 
 int main()
 {
     //parte esquerda da árvore
-    Arv* a1 = criaArv(4,criaVazia(),criaVazia);
-    Arv* a2 = criaArv(7,criaVazia(),criaVazia);
+    //Aqui eu deixei como null pq tava funcionando dessa forma
+    Arv* a1 = criaArv(4,NULL,NULL);
+    Arv* a2 = criaArv(7,NULL,NULL);
     Arv* a3 = criaArv(6,a1,a2);
-    Arv* a4 = criaArv(1,criaVazia(),criaVazia);
+    Arv* a4 = criaArv(1,NULL,NULL);
     Arv* a5 = criaArv(3,a4,a3);
     //parte direita da árvore
-    Arv* a6 = criaArv(13,criaVazia(),criaVazia);
-    Arv* a7 = criaArv(14,a6,criaVazia());
-    Arv* a8 = criaArv(10,a7,criaVazia());
+    Arv* a6 = criaArv(13,NULL,NULL);
+    Arv* a7 = criaArv(14,a6,NULL);
+    Arv* a8 = criaArv(10,a7,NULL);
     //nó raiz
     Arv* raiz = criaArv(8,a5,a8);
-}
-
+	imprime(a1);
+	imprime(a5);
+	imprime(raiz);
+	
+    //buscaMin(raiz); < essa funçao ta bugada
+};
